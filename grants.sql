f63150def5a434ce97e593a3ed28c99be9c1cc2f
MarcoBackman <roni2006@hanmail.com>
2022-08-14 10:44:34 -0400

    Grant details updated

--- grants.sql
+++ grants.sql
@@ -5,3 +5,15 @@ CREATE USER 'team_agile_alan'@'%' IDENTIFIED BY '';
 GRANT ALL PRIVILEGES ON Authentication.* TO 'team_agile_leo'@'%';
 GRANT ALL PRIVILEGES ON Authentication.* TO 'team_agile_yang'@'%';
 GRANT ALL PRIVILEGES ON Authentication.* TO 'team_agile_alan'@'%';
+
+GRANT ALL PRIVILEGES ON HousingService.* TO 'team_agile_leo'@'%';
+GRANT ALL PRIVILEGES ON HousingService.* TO 'team_agile_yang'@'%';
+GRANT ALL PRIVILEGES ON HousingService.* TO 'team_agile_alan'@'%';
+
+GRANT ALL PRIVILEGES ON ApplicationService.* TO 'team_agile_leo'@'%';
+GRANT ALL PRIVILEGES ON ApplicationService.* TO 'team_agile_yang'@'%';
+GRANT ALL PRIVILEGES ON ApplicationService.* TO 'team_agile_alan'@'%';
+
+SHOW DATABASES;
+
+select * from mysql. user;
